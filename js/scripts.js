6e2b66e11db7a1021200008ce5d4a146d4331601
Celine Italia <celineitalia@yahoo.com>
2023-05-22 20:58:03 -0500

    added pokemon types and created forEach function
    -add "bug" and "flying" pokemon types to 'typeArray'
    -combined 'pokemonList2' arrays to 'pokemonList'
    -created 'forEach()' function instead of 'for' loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,23 @@
-let typeArray = ["fire", "grass", "water"];
+let typeArray = ["bug", "fire", "flying", "grass", "water"];
 
 let pokemonList = [
-  { name: "Bulbasaur", height: 28, type: typeArray[1] },
-  { name: "Charmander", height: 24, type: typeArray[0] },
-  { name: "Squirtle", height: 20, type: typeArray[2] },
+  { name: "Bulbasaur", height: 28, type: typeArray[3] },
+  { name: "Charmander", height: 24, type: typeArray[1] },
+  { name: "Squirtle", height: 20, type: typeArray[4] },
+  { name: "Caterpie", height: 12, type: typeArray[0] },
+  { name: "Weedle", height: 12, type: typeArray[0] },
+  { name: "Pidgey", height: 12, type: typeArray[2] },
 ];
 
-// 'for' loop that iterates over each Pokemon in 'pokemonList' array
-for (let i = 0; i < pokemonList.length; i++) {
-  // conditional that highlights tall Pokemon
-  if (pokemonList[i].height >= 24) {
-    document.write(
-      pokemonList[i].name +
-        " (height: " +
-        pokemonList[i].height +
-        " inches)" +
-        " - Wow, that's a tall Pokemon!" +
-        "<br>"
-    );
-  } else {
-    document.write(
-      pokemonList[i].name +
-        " (height: " +
-        pokemonList[i].height +
-        " inches)" +
-        "<br>"
-    );
-  }
-}
+pokemonList.forEach(function (pokemon) {
+  document.write(
+    "<p>" +
+      pokemon.name +
+      " (height: " +
+      pokemon.height +
+      " inches, type: " +
+      pokemon.type +
+      ")" +
+      "</p>"
+  );
+});
