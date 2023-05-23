d65a1e61db6aae8fd05c0424061c9802cc61a917
Celine Italia <celineitalia@yahoo.com>
2023-05-22 22:26:51 -0500

    wrapped 'pokemonList' array in IIFE
    -wrapped 'pokemonList' array in IIFE
    -created 'pokemonRepository' variable
    -assigned IIFE to 'pokemonRepository' variable
    -created 'getAll' and 'add' functions for IIFE
    -updated 'forEach()' loop to retrieve 'pokemonList' array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 let typeArray = ["bug", "fire", "flying", "grass", "water"];
 
-let pokemonList = [
-  { name: "Bulbasaur", height: 28, type: typeArray[3] },
-  { name: "Charmander", height: 24, type: typeArray[1] },
-  { name: "Squirtle", height: 20, type: typeArray[4] },
-  { name: "Caterpie", height: 12, type: typeArray[0] },
-  { name: "Weedle", height: 12, type: typeArray[0] },
-  { name: "Pidgey", height: 12, type: typeArray[2] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 28, type: typeArray[3] },
+    { name: "Charmander", height: 24, type: typeArray[1] },
+    { name: "Squirtle", height: 20, type: typeArray[4] },
+    { name: "Caterpie", height: 12, type: typeArray[0] },
+    { name: "Weedle", height: 12, type: typeArray[0] },
+    { name: "Pidgey", height: 12, type: typeArray[2] },
+  ];
 
-pokemonList.forEach(function (pokemon) {
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    },
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   document.write(
     "<p>" +
       pokemon.name +
